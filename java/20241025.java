dfae0beef92114b3bc540813e9579f46bff633f5
Jamy <63951751+Park-Jamy@users.noreply.github.com>
2024-10-26 01:41:50 +0900

    Update 20241025

--- java/20241025.java
+++ java/20241025.java
@@ -83,3 +83,77 @@ public class Main{
         br.close();
     }
 }
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String ss = br.readLine();
+
+    int ss_len = ss.length();
+    StringBuffer sb = new StringBuffer();
+    StringBuffer sb1 = new StringBuffer();
+
+    for (int i = 0; i < ss_len; i++) {
+      if (ss.charAt(i) == '<') {
+        String str_mid = calc(sb1.toString());
+        sb.append(str_mid);
+        
+        sb.append("<");
+
+        while (true) {
+          i++;
+          if (ss.charAt(i) == '>') {
+            sb.append(">");
+            sb1 = new StringBuffer();
+            break;
+          }
+          sb.append(ss.charAt(i));
+        }
+      } else {
+        sb1.append(ss.charAt(i));
+      }
+      if(i == ss_len - 1){
+        String str_mid = calc(sb1.toString());
+        sb.append(str_mid);
+      }
+    }
+
+    System.out.println(sb.toString());
+  }
+
+  private static String calc(String str) {
+    StringBuffer sb = new StringBuffer();
+
+    int str_len = str.length();
+
+    String[] str_arr = str.split(" ");
+
+    if(str_arr.length > 1){
+      for(int i = 0; i < str_arr.length; i++){
+        //StringBuffer sb1 = new StringBuffer();
+
+        String now_str = str_arr[i];
+
+        int now_str_len = now_str.length();
+
+        for (int j = 0; j < now_str_len; j++) {
+          sb.append(now_str.charAt(now_str_len - j - 1));
+        }
+
+        if(i != str_arr.length - 1){
+          sb.append(" ");
+        }
+      }
+    }else{
+      for (int i = 0; i < str_len; i++) {
+        sb.append(str.charAt(str_len - i - 1));
+      }
+    }
+    
+
+    return sb.toString();
+  }
+}
