1bbfbe444b1fa2ad72429d73dd296af2936f0044
Jamy <63951751+Park-Jamy@users.noreply.github.com>
2024-10-26 15:07:34 +0900

    Update 20241026.java

--- java/20241026.java
+++ java/20241026.java
@@ -79,3 +79,67 @@ public class Main {
     }
   }
 }
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  private static int[][] map;
+  private static int[] list;
+  //private static 
+  
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
+    map = new int[n+1][n+1];
+    list = new int[n+1];
+    list[0] = -1;
+    
+    for(int i = 0; i < n - 1; i++){
+      String[] nums = br.readLine().split(" ");
+      int x = Integer.parseInt(nums[0]);
+      int y = Integer.parseInt(nums[1]);
+
+      map[x][y] = map[y][x] = 1;
+    }
+
+    StringBuffer sb = new StringBuffer();
+    
+    boolean[][] visited = new boolean[n+1][n+1];
+    find(n, 1, visited);
+    
+    // 2번 부터 부모 노드 확인
+    for(int i = 2; i < n + 1; i++){
+      sb.append(list[i]).append("\n");
+    }
+
+    System.out.println(sb.toString());
+  }
+
+  private static void find(int n, int parent, boolean[][] visited){
+
+    // if(list[sun] != 0) return; // 이미 부모 노드를 찾은 경우
+    // else{
+    //   if(map[parent][sun] == 1){
+    //     list[sun] = parent;
+    //     return;
+    //   }else{
+        for(int i = 1; i < n + 1; i++){
+          
+          if(visited[parent][i]) continue;
+          
+          if(map[parent][i] != 1) continue;
+          
+          if(list[i] != 0) 
+          list[i] = parent;
+          
+          visited[parent][i] = visited[i][parent] = true;
+
+          find(n, i, visited);
+        }
+      // }
+    // }
+  }
+}
